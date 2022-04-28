5afbafda2d5f9239c8a18cc7b458029c888f6e84
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-04-29 01:22:15 +0900

    [feat] https 연결

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const connect = require("./schemas/index");
 const Router = require("./routers/index");
 const cors = require("cors");
-const app = express();
 const port = 3000;
+const app = express();
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
 
 connect();
 
@@ -20,6 +23,42 @@ app.get(
   }
 );
 
-app.listen(port, () => {
-  console.log(port, "Server is listening...");
+//인증서
+const privateKey = fs.readFileSync(__dirname + '/private.key', "utf8");
+const certificate = fs.readFileSync(__dirname + '/certificate.crt', "utf8");
+const ca = fs.readFileSync(__dirname + '/ca_bundle.crt', 'utf8');
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
+
+const app_low = express();
+
+
+const httpPort = 80;
+const httpsPort = 443;
+
+app_low.use((req, res, next) => {
+  if (req.secure) {
+    next();
+  } else {
+    const to = `https://${req.hostname}:${httpsPort}${req.url}`;
+    console.log(to);
+    res.redirect(to);
+  }
 });
+
+http.createServer(app_low).listen(httpPort, () => {
+  console.log('http서버 켜짐');
+});
+
+https.createServer(credentials, app).listen(httpsPort, () => {
+  console.log('https서버 켜짐');
+});
+
+
+
+// app.listen(port, () => {
+//   console.log(port, "Server is listening...");
+// });
