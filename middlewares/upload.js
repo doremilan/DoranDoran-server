73f916003f9f4f410ed35bb0087609f8f990a066
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-05-03 03:57:47 +0900

    [chore] middleware 폴더이동

---
+++ middlewares/upload.js
@@ -0,0 +1,28 @@
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+require("dotenv").config();
+
+const s3 = new aws.S3({
+    accessKeyId: S3_ACCESS_KEY,
+    secretAccessKey: S3_SECRET_ACCESS_KEY,
+    region: S3_BUCKET_REGION,
+});
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: S3_BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString());
+        },
+    }),
+});
+
+
+module.exports = { upload }
