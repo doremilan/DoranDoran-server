103e8eb1956ec6204e8c2c1a741b9f68e218d995
doremilan <yooossi@gmail.com>
2022-05-03 19:46:33 +0900

    [feat] 미션목록조회 수정
    [feat] 미션목록조회 수정

--- routers/mission.js
+++ routers/mission.js
@@ -18,19 +18,7 @@ router.post("/:missionId", authMiddleware, completeMission);
 // 이번주 미션 목록조회
 router.get("/:familyId", authMiddleware, getMission);
 
-// 이번주 미션 현황
-router.get("/:familyId", authMiddleware, getMission);
-
 // 지난 미션 목록조회
 router.get("/:familyId/pastmission", authMiddleware, getPastMission);
-
-// 앨범삭제
-router.delete("/:familyId", authMiddleware, deleteMission);
-
-// 사진 목록조회
-router.get("/:photoAlbumId", authMiddleware, getMission);
-
-// 사진 상세조회
-router.get("/:photoId", authMiddleware, getMission);
 
 module.exports = router;
