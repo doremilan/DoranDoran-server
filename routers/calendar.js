fd3a3110a2dad23802938d1e92b8b7927914aa51
doremilan <yooossi@gmail.com>
2022-05-04 00:43:46 +0900

    [feat] 테스트 수정
    [feat] 테스트 수정

--- routers/calendar.js
+++ routers/calendar.js
@@ -1,30 +1,32 @@
-const express = require("express");
-const router = express.Router();
-const Middleware = require("../middlewares/authMiddleware");
+// const express = require("express");
+// const router = express.Router();
+// const Middleware = require('../middlewares/authMiddleware');
 
-const {
-  createEvent,
-  updateEvent,
-  deleteEvent,
-  getEvent,
-} = require("../controllers/eventController");
+// const {
+//     createEvent,
+//     updateEvent,
+//     deleteEvent,
+//     getEvent,
+// } = require('../controllers/eventController')
 
-//일정보기 API
-router.get("/calendar/:familyId/eventcalendar/:date", getEvent);
 
-//추억보기 API
-router.get("/calendar/:familyId/photocalendar/:date");
+// //일정보기 API
+// router.get('/calendar/:familyId/eventcalendar/:date', getEvent)
 
-//추억 상세보기 API
-router.get("/calendar/:familyId/photocalendar/detail/:date");
+// //추억보기 API
+// router.get('/calendar/:familyId/photocalendar/:date')
 
-//일정 작성 API
-router.get("/calendar/:familyId", createEvent);
+// //추억 상세보기 API
+// router.get('/calendar/:familyId/photocalendar/detail/:date')
 
-//일정 수정 API
-router.get("/calendar/:eventId", updateEvent);
+// //일정 작성 API
+// router.get('/calendar/:familyId', createEvent)
 
-//일정 삭제 API
-router.get("/calendar/:eventId", deleteEvent);
+// //일정 수정 API
+// router.get('/calendar/:eventId', updateEvent)
 
-module.exports = router;
+// //일정 삭제 API
+// router.get('/calendar/:eventId', deleteEvent)
+
+
+// module.exports = router;
--- routers/user.js
+++ routers/user.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../middlewares/authMiddleware");
-const jwt = require("jsonwebtoken");
-const fs = require("fs");
-require("dotenv").config();
+// const express = require("express");
+// const router = express.Router();
+// const authMiddleware = require("../middlewares/authMiddleware");
+// const jwt = require("jsonwebtoken");
+// const fs = require("fs");
+// require("dotenv").config();
 
-const {
-  getUser,
-  signup,
-  login,
-  getProfile,
-  editProfile,
-  editTodayMood,
-} = require("../controllers/userController");
+// const {
+//   getUser,
+//   signup,
+//   login,
+//   getProfile,
+//   editProfile,
+//   editTodayMood,
+// } = require("../controllers/userController");
 
-router.get("/me", authMiddleware, getUser);
+// router.get("/me", authMiddleware, getUser);
 
-//프로필 조회 API
-router.get("/myprofile", authMiddleware, getProfile);
+// //프로필 조회 API
+// router.get("/myprofile", authMiddleware, getProfile);
 
-//프로필 수정 API
-router.put("/myPfofile", authMiddleware, editProfile);
+// //프로필 수정 API
+// router.put("/myPfofile", authMiddleware, editProfile);
 
-//오늘의 기분 수정 API
-router.put("/myPfofile/todaymood", authMiddleware, editTodayMood);
+// //오늘의 기분 수정 API
+// router.put("/myPfofile/todaymood", authMiddleware, editTodayMood);
 
-module.exports = router;
+// module.exports = router;
