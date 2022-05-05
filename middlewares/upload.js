ceb2f23d3dbbb39f11bc3377fd0a7ace1524ab4e
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-05-05 21:44:30 +0900

    [chore] multer 업로드 경로변경

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const AWS = require('aws-sdk');
@@ -10,20 +11,26 @@ const s3 = new AWS.S3({
 });
 
 
+
+
 const upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: 'practice2082',
+        bucket: 'family-8',
         contentType: multerS3.AUTO_CONTENT_TYPE,
         acl: 'public-read',
         metadata: function (req, file, cb) {
             cb(null, { fieldName: file.fieldname });
         },
         key: function (req, file, cb) {
-            cb(null, Date.now().toString());
+            if (file.fieldname === "voiceFile")
+                cb(null, `voice/${Date.now()}${path.basename(file.originalname)}`);
+            if (file.fieldname === "photo")
+                cb(null, `photo/${Date.now()}${path.basename(file.originalname)}`);
         },
     }),
 });
+
 
 
 module.exports = upload;
