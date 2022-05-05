a957f061c30a50ffa697add7f2274ce34e0fabbc
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-05-05 21:45:46 +0900

    [chere] voiceFile 컨트롤러 수정

--- controllers/voiceFileController.js
+++ controllers/voiceFileController.js
@@ -7,45 +7,48 @@ const User = require("../schemas/user");
 
 //음성파일 생성
 const createVoiceFile = async (req, res) => {
-    const { voiceAlbumId } = req.params;
-    const { voiceFileTilte, voicePlayTime } = req.body;
+    const { familyId, voiceAlbumId } = req.params;
+    const { userId, voiceTitle, voicePlayTime } = req.body;
     // const { userId } = res.locals;
-    const { voiceFile } = req.file.location;
+    const voiceFile = req.file.location;
     const createdAt = new Date();
-    const familyMemberNickname = await FamilyMember.findOne({
-        familyMemberNickname,
-    });
-    console.log(voiceFileTilte.voicePlayTime)
-    const profileImg = await FamilyMember.findOne({ profileImg });
-
-    // console.log(voiceAlbumId)
-    // console.log(voiceFile, voicePlayTime)
-    // console.log(userId)
-
-    const createVoice = voiceFile.create({
-        voiceAlbumId,
-        voiceFileTilte,
-        voiceFile,
-        voicePlayTime,
-        familyMemberNickname,
-        profileImg,
-        createdAt,
-    });
-    res.status(201).json({
-        createVoice,
-        msg: "음성메세지 추가되었습니다.",
-    });
+    try {
+        const userInfo = await FamilyMember.findOne({ userId });
+        const familyMemberNickname = userInfo.familyMemberNickname
+        const profileImg = userInfo.profileImg
+        console.log(familyMemberNickname, profileImg)
+        const createVoice = await VoiceFile.create({
+            voiceAlbumId,
+            voiceTitle,
+            voiceFile,
+            voicePlayTime,
+            familyId,
+            familyMemberNickname,
+            profileImg,
+            createdAt,
+        });
+        res.status(201).json({
+            createVoice,
+            msg: "음성메세지 추가되었습니다.", z
+        });
+    } catch (error) {
+        res.status(400).send({
+            msg: "에러"
+        })
+        console.log(error)
+    }
 };
 
 // 음성파일 조회
 const getVoiceFile = async (req, res) => {
     const { voiceAlbumId } = req.params;
     // const { userId } = res.locals.user;
-
+    console.log(11, voiceAlbumId)
     try {
-        const [photoList] = await Photo.find({ voiceAlbumId }).sort("-createdAt");
+        const voiceFileList = await VoiceFile.find({})  // 연결안되는중
+        console.loe(22, voiceFileList)
         res.status(200).json({
-            photoList,
+            voiceFileList,
         });
     } catch (error) {
         res.status(400).send({
