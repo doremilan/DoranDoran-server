f5ded7cd7c9cebdddb40a90aa3a6bd79b5a20bcf
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-05-06 21:32:51 +0900

    [chore] ssl 인증서부분 삭제

--- app.js
+++ app.js
@@ -8,13 +8,6 @@ const app = express();
 const port = 3000;
 const indexRouter = require("./routers/index");
 
-// const eventRouter = require('./routers/calendar')
-
-// app.use("/api", eventRouter);
-
-// const http = require('http') // https 연결
-// const https = require('https') // https 연결
-
 connect();
 
 // 각종 미들웨어
@@ -38,50 +31,6 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
-// https 연결코드
-// app.get(
-//   "/.well-known/pki-validation/AD424447A620C77C679BD83C2FA6E5BC.txt",
-//   (req, res) => {
-//     res.sendFile(__dirname +
-//       "/well-known/pki-validation/AD424447A620C77C679BD83C2FA6E5BC.txt");
-//   }
-// );
-
-// https 인증서
-// const privateKey = fs.readFileSync(__dirname + '/private.key', "utf8");
-// const certificate = fs.readFileSync(__dirname + '/certificate.crt', "utf8");
-// const ca = fs.readFileSync(__dirname + '/ca_bundle.crt', 'utf8');
-// const credentials = {
-//   key: privateKey,
-//   cert: certificate,
-//   ca: ca,
-// };
-
-// const app_low = express();
-
-// https 연결코드
-// const httpPort = 3000;
-// const httpsPort = 443;
-
-// https 연결코드
-// app_low.use((req, res, next) => {
-//   if (req.secure) {
-//     next();
-//   } else {
-//     const to = `https://${req.hostname}:${httpsPort}${req.url}`;
-//     console.log(to);
-//     res.redirect(to);
-//   }
-// });
-
-// https 연결코드
-// http.createServer(app_low).listen(httpPort, () => {
-//   console.log('http서버 켜짐');
-// });
-
-// https.createServer(credentials, app).listen(httpsPort, () => {
-//   console.log('https서버 켜짐');
-// });
 
 // 서버 열기
 app.listen(port, () => {
