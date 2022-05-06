2639aa1f4ca4e2aeffd9943d4c309c4750127499
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-05-06 21:28:23 +0900

    [chore] ssl인증서부분 삭제

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const cors = require("cors");
 const port = 3000;
 const app = express();
 const fs = require('fs')
-// const http = require('http')
-// const https = require('https')
 
 connect();
 
@@ -23,21 +21,7 @@ app.get(
   }
 );
 
-//인증서
-// const privateKey = fs.readFileSync(__dirname + '/private.key', "utf8");
-// const certificate = fs.readFileSync(__dirname + '/certificate.crt', "utf8");
-// const ca = fs.readFileSync(__dirname + '/ca_bundle.crt', 'utf8');
-// const credentials = {
-//   key: privateKey,
-//   cert: certificate,
-//   ca: ca,
-// };
-
 const app_low = express();
-
-
-// const httpPort = 3000;
-// const httpsPort = 443;
 
 app_low.use((req, res, next) => {
   if (req.secure) {
@@ -48,14 +32,6 @@ app_low.use((req, res, next) => {
     res.redirect(to);
   }
 });
-
-// http.createServer(app_low).listen(httpPort, () => {
-//   console.log('http서버 켜짐');
-// });
-
-// https.createServer(credentials, app).listen(httpsPort, () => {
-//   console.log('https서버 켜짐');
-// });
 
 
 
