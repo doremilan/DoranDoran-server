b288618efb92585f5e90d8d57ca9d078af6332b6
doremilan <yooossi@gmail.com>
2022-05-08 01:18:53 +0900

    [chore] 디비 연결코드 수정

--- schemas/index.js
+++ schemas/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
+
+const dotenv = require('dotenv')
+dotenv.config({ path: __dirname + '/.env' })
 
 const connect = () => {
   mongoose
