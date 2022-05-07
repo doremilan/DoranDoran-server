dc7f9bfcc9eccc17db0d5f9c5361c2d9ea87c23a
doremilan <yooossi@gmail.com>
2022-05-08 01:22:39 +0900

    [chore] db연결 코드수정
    [chore] db연결 코드수정

--- schemas/index.js
+++ schemas/index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({ path: __dirname + '/.env' })
 
+console.log(process.env.DB_NAME)
+
 const connect = () => {
   mongoose
     .connect(process.env.DB_NAME, { ignoreUndefined: true })
