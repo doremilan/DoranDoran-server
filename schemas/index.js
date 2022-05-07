68acad26d961751739b9c98640015206a1af5bb8
doremilan <yooossi@gmail.com>
2022-05-08 01:34:46 +0900

    [chore] db연결 수정
    [chore] db연결 수정

--- schemas/index.js
+++ schemas/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
+
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
 const connect = () => {
   mongoose
