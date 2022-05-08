9cdff3a436e333398c4cda352ed4a127f1186fb7
doremilan <yooossi@gmail.com>
2022-05-08 20:30:01 +0900

    [chore] 서버연결 코드수정
    [chore] 서버연결 코드수정

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-const express = require('express');
-const indexRouter = require('./routers/index');
-const connect = require('./schemas/index');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const fs = require('fs');
-const port = 3000;
-const app = express();
+const express = require('express')
+const indexRouter = require('./routers/index')
+const connect = require('./schemas/index')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const fs = require('fs')
+const port = 3000
+const app = express()
 
-connect();
+connect()
 
 // 각종 미들웨어
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(helmet()); //보안에 필요한 헤더 추가 미들웨어
-app.use(morgan('tiny')); // 서버 요청 모니터링 미들웨어
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(helmet()) //보안에 필요한 헤더 추가 미들웨어
+app.use(morgan('tiny')) // 서버 요청 모니터링 미들웨어
 
 // 라우터 연결
-app.use(indexRouter);
+app.use(indexRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  res.sendStatus(404);
-});
+  res.sendStatus(404)
+})
 
 // error handler
 app.use((error, req, res, next) => {
-  console.error(error);
-  res.sendStatus(500);
-});
+  console.error(error)
+  res.sendStatus(500)
+})
 
 // 서버 열기
+app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => {
-  console.log(port, 'Server is listening...');
-});
+  console.log(port, 'Server is listening...')
+})
