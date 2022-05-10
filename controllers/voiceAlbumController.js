9541e157c1518f4a7f05295021728856981e7dfd
doremilan <yooossi@gmail.com>
2022-05-11 00:33:38 +0900

    [chore] 보이스앨범 수정

--- controllers/voiceAlbumController.js
+++ controllers/voiceAlbumController.js
@@ -1,5 +1,5 @@
-const VoiceAlbum = require('../schemas/voiceAlbum')
-const VoiceFile = require('../schemas/voiceFile')
+const VoiceAlbum = require("../schemas/voiceAlbum")
+const VoiceFile = require("../schemas/voiceFile")
 
 //보이스 앨범 생성
 const createVoiceAlbum = async (req, res) => {
@@ -11,7 +11,7 @@ const createVoiceAlbum = async (req, res) => {
 
   try {
     // 공백 체크
-    if (voiceAlbumName !== null && voiceAlbumName !== '') {
+    if (voiceAlbumName !== null && voiceAlbumName !== "") {
       const createdvoiceAlbum = await VoiceAlbum.create({
         familyId,
         userId,
@@ -24,19 +24,19 @@ const createVoiceAlbum = async (req, res) => {
       })
       res.status(201).json({
         voiceAlbumId: newvoiceAlbumId._id,
-        msg: '새로운 앨범이 생성되었어요.',
+        msg: "새로운 앨범이 생성되었어요.",
       })
     } else {
       res.status(400).send({
         result: false,
-        msg: '앨범 이름을 작성해주세요.',
+        msg: "앨범 이름을 작성해주세요.",
       })
     }
   } catch (error) {
-    console.log('앨범 생성 오류', error)
+    console.log("앨범 생성 오류", error)
     res.status(400).send({
       result: false,
-      msg: '앨범 생성 실패',
+      msg: "앨범 생성 실패",
     })
   }
 }
@@ -47,17 +47,17 @@ const getVoiceAlbum = async (req, res) => {
 
   try {
     const voiceAlbumList = await VoiceAlbum.find({ familyId }).sort(
-      '-createdAt'
+      "-createdAt"
     )
 
     res.status(200).json({
       voiceAlbumList,
     })
   } catch (error) {
-    console.log('보이스앨범 조회 오류', error)
+    console.log("보이스앨범 조회 오류", error)
     res.status(400).send({
       result: false,
-      msg: '보이스앨범 조회 실패',
+      msg: "보이스앨범 조회 실패",
     })
   }
 }
@@ -65,10 +65,10 @@ const getVoiceAlbum = async (req, res) => {
 //보이스 앨범 수정
 const updateVoiceAlbum = async (req, res) => {
   const { voiceAlbumId } = req.params
-  const { voiceAlbumName } = req.body
+  const { voiceAlbumName, voiceAlbumCover } = req.body
   // console.log(voiceAlbumId, voiceAlbumName);
   try {
-    if (voiceAlbumName !== null && voiceAlbumName !== '') {
+    if (voiceAlbumName !== null && voiceAlbumName !== "") {
       const existVoiceAlbum = await VoiceAlbum.findOne({ _id: voiceAlbumId })
       console.log(existVoiceAlbum)
       if (existVoiceAlbum) {
@@ -80,19 +80,19 @@ const updateVoiceAlbum = async (req, res) => {
       res.status(200).json({
         voiceAlbumName,
         voiceAlbumCover,
-        msg: '앨범이 수정되었습니다.',
+        msg: "앨범이 수정되었습니다.",
       })
     } else {
       res.status(400).send({
         result: false,
-        msg: '앨범 이름을 작성해주세요.',
+        msg: "앨범 이름을 작성해주세요.",
       })
     }
   } catch (error) {
-    console.log('보이스 앨범 수정 오류', error)
+    console.log("보이스 앨범 수정 오류", error)
     res.status(400).send({
       result: false,
-      msg: '앨범 수정을 실패했습니다.',
+      msg: "앨범 수정을 실패했습니다.",
     })
   }
 }
@@ -111,14 +111,14 @@ const deleteVoiceAlbum = async (req, res) => {
 
       res.status(204).json({
         result: true,
-        msg: '앨범이 성공적으로 삭제되었습니다.',
+        msg: "앨범이 성공적으로 삭제되었습니다.",
       })
     }
   } catch (error) {
-    console.log('보이스앨범 삭제 오류', error)
+    console.log("보이스앨범 삭제 오류", error)
     res.status(400).send({
       result: false,
-      msg: '앨범삭제에 실패하였습니다.',
+      msg: "앨범삭제에 실패하였습니다.",
     })
   }
 }
