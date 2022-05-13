b9a2a0f1bc65d7c92e8df37a01e4b7b4aab2845f
SEOCL <com5045@gmail.com>
2022-05-13 20:46:34 +0900

    [chore] 수정
    수정

--- routers/user.js
+++ routers/user.js
@@ -1,23 +1,24 @@
-const express = require('express')
-const router = express.Router()
-const authMiddleware = require('../middlewares/authMiddleware')
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const {
   getUser,
   getProfile,
   editProfile,
   editTodayMood,
-} = require('../controllers/userController')
+} = require("../controllers/userController");
 
-router.get('/me', authMiddleware, getUser)
+////유저 데이터 get API
+router.get("/me", authMiddleware, getUser);
 
 //프로필 조회 API
-router.get('/myprofile', authMiddleware, getProfile)
+router.get("/myprofile", authMiddleware, getProfile);
 
 //프로필 수정 API
-router.put('/myprofile', authMiddleware, editProfile)
+router.put("/myprofile", authMiddleware, editProfile);
 
 //오늘의 기분 수정 API
-router.put('/myprofile/todaymood', authMiddleware, editTodayMood)
+router.put("/myprofile/todaymood", authMiddleware, editTodayMood);
 
-module.exports = router
+module.exports = router;
