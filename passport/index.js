27c9ee5419deab829090f1b2b4a788c78bfd41f1
doremilan <yooossi@gmail.com>
2022-05-15 16:33:08 +0900

    [fix] 카카오로그인 수정

--- passport/index.js
+++ passport/index.js
@@ -12,20 +12,20 @@ module.exports = (app) => {
       },
       async (accessToken, refreshToken, profile, done) => {
         console.log(1, profile)
+        console.log(2, profile.id)
+        console.log(3, profile._json.id)
         try {
           const exUser = await User.findOne({
-            // 카카오 플랫폼에서 로그인 했고 & snsId필드에 카카오 아이디가 일치할경우
-            snsId: profile.id,
+            snsId: profile._json.id,
           })
-          // 이미 가입된 카카오 프로필이면 성공
           if (exUser) {
             done(null, exUser) // 로그인 인증 완료
           } else {
             // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
             const newUser = await User.create({
-              email: profile._json && profile._json.kakao_account_email,
+              email: profile._json.kakao_account.email,
               nickname: profile.displayName,
-              // profileImg: 콘솔 확인후 작성
+              profileImg: profile._json.profile_image,
               snsId: profile.id,
               provider: "kakao",
             })
