98a93782cbac28f6bae62da8505550fb324a2eef
doremilan <yooossi@gmail.com>
2022-05-16 17:55:38 +0900

    [fix] 카카오 로그인 수정

--- controllers/authController.js
+++ controllers/authController.js
@@ -191,24 +191,20 @@ const kakaoCallback = (req, res, next) => {
     }
     const token = jwt.sign({ email: user.email }, config.jwt.secretKey, options)
     // 유저의 가족 리스트 추출
+    const familyChk = FamilyMember.find({ userId: user._id })
     let familyList = []
-    FamilyMember.find({ userId: user._id })
-      .exec()
-      .then((familyChk) => {
-        if (familyChk.length) {
-          for (let family of familyChk) {
-            const Checkedfamily = Family.findOne({ _id: family.familyId })
-            familyList.push(Checkedfamily)
-          }
-        }
-      })
+    if (familyChk.length) {
+      for (let family of familyChk) {
+        const Checkedfamily = Family.findOne({ _id: family.familyId })
+        familyList.push(Checkedfamily)
+      }
+    }
     res.json({
       token,
       user,
       familyList,
     })
   })(req, res, next)
-  next()
 }
 
 module.exports = {
