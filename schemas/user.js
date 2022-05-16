b06a8bd348f1d8e187a2b2975b13d716a4f18068
doremilan <yooossi@gmail.com>
2022-05-16 10:45:36 +0900

    [fix] 카카오 로그인 수정

--- schemas/user.js
+++ schemas/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const userSchema = mongoose.Schema({
   email: {
     type: String,
-    // required: true,
+    required: true,
   },
   password: {
     type: String,
