d0f92dd72a98d549f344c87e9b94e289c7554c4a
doremilan <yooossi@gmail.com>
2022-05-16 11:13:06 +0900

    [fix] 카카오 수정

--- controllers/authController.js
+++ controllers/authController.js
@@ -186,18 +186,18 @@ const kakaoCallback = (req, res, next) => {
       if (err) return next(err)
       const token = jwt.sign({ snsId: user.snsId }, config.jwt.secretKey)
       // 유저의 가족 리스트 추출
-      const familyChk = FamilyMember.find({ userId: user._id })
-      let familyList = []
-      if (familyChk.length) {
-        for (let family of familyChk) {
-          const Checkedfamily = Family.findOne({ _id: family.familyId })
-          familyList.push(Checkedfamily)
-        }
-      }
+      // const familyChk = FamilyMember.find({ userId: user._id })
+      // let familyList = []
+      // if (familyChk.length) {
+      //   for (let family of familyChk) {
+      //     const Checkedfamily = Family.findOne({ _id: family.familyId })
+      //     familyList.push(Checkedfamily)
+      //   }
+      // }
       res.json({
         token,
         user,
-        familyList,
+        // familyList,
       })
     })(req, res, next)
   } catch (error) {
