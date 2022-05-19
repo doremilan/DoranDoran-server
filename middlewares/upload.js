993cc41af950e059b8ff67b4eb8e08914384b22f
doremilan <yooossi@gmail.com>
2022-05-20 01:21:50 +0900

    [fix] 콘솔로그 추가

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -24,7 +24,10 @@ const upload = multer({
         cb(null, `voice/${Date.now()}${path.basename(file.originalname)}`)
       if (file.fieldname === "photoFile")
         cb(null, `photo/${Date.now()}${path.basename(file.originalname)}`)
-      console.log(1111, cb)
+      console.log(
+        1111,
+        `voice/${Date.now()}${path.basename(file.originalname)}`
+      )
     },
   }),
 })
