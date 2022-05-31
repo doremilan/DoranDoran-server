1f204b961b62cf9645d68ebc8266ed95c65059d4
SEOCL <com5045@gmail.com>
2022-06-01 00:04:36 +0900

    [featch] auth 테스트코드 수정, user 테스트 코드 시작
    [featch] auth 테스트코드 수정, user 테스트 코드 시작

--- test/unit/authController.test.js
+++ test/unit/authController.test.js
@@ -1,7 +1,11 @@
+jest.mock("jsonwebtoken")
+import jwt from "jsonwebtoken"
+
 const authController = require("../controllers/authController")
 const httpMocks = require("node-mocks-http")
-const locals = require("../data/locals.json")
 const User = require("../schemas/user")
+const FamilyMember = require("../schemas/familyMember")
+const Family = require("../schemas/family")
 
 //json으로 만든 가짜 프론트 req.body 데이터 값.
 const signup1 = require("../data/signup1.json")
@@ -14,16 +18,19 @@ const signup8 = require("../data/signup8.json")
 const wrongPW = require("../data/wrongPW.json")
 const isUser = require("../data/isUser.json")
 const isPW = require("../data/isPW.json")
+const isMember = require("../data/isMember.json")
+const isFamily = require("../data/isFamily.json")
 
 User.findOne = jest.fn()
 User.create = jest.fn()
 User.updateOne = jest.fn()
+Family.findOne = jest.fn()
+FamilyMember.find = jest.fn()
 
 beforeEach(() => {
   req = httpMocks.createRequest()
   res = httpMocks.createResponse()
   next = null
-  res.locals.user = locals
 })
 
 //회원가입 테스트
@@ -138,6 +145,35 @@ describe("로그인", () => {
       expect(res.statusCode).toBe(200)
       expect(res._isEndCalled()).toBeTruthy()
       // 매번 토큰 값이 달라져 JSONData로 결과값 확인 어려움
+    })
+
+    test("로그인 완료 시, 프론트가 요청한 값을 보낸다.", async () => {
+      req.body = isPW
+
+      const payload = { email }
+      const options = {
+        issuer: "백엔드 개발자",
+        expiresIn: "2d",
+      }
+      const token = jwt.sign(payload, config.jwt.secretKey, options)
+      const userInfo = await User.findOne.mockResolvedValue(isUser)
+      const familyChk = await FamilyMember.find.mockResolvedValue(isMember)
+      let familyList = []
+      if (familyChk.length) {
+        for (let family of familyChk) {
+          const chechkedFamily = await Family.findOne.mockResolvedValue(isFamily)
+          familyList.push(chechkedFamily)
+        }
+      }
+
+      await authController.login(req, res, next)
+
+      expect(res._getJSONData()).toStrictEqual({
+        logIntoken: token,
+        userInfo,
+        familyList,
+        msg: "로그인이 완료되었습니다.",
+      })
     })
   })
 })
---
+++ test/unit/userController.test.js
@@ -0,0 +1,85 @@
+const userController = require("../controllers/userController")
+const httpMocks = require("node-mocks-http")
+const User = require("../schemas/user")
+const Family = require("../schemas/family")
+const FamilyMember = require("../schemas/familyMember")
+const MissionMember = require("../schemas/missionMember")
+const Event = require("../schemas/event")
+const VoiceFile = require("../schemas/voiceFile")
+
+////json으로 만든 가짜 프론트 req.body 데이터 값.
+const isUser = require("../data/isUser.json")
+const isMember = require("../data/isMember.json")
+const isFamily = require("../data/isFamily.json")
+const locals = require("../data/locals.json")
+const photoFile = require("../data/photoFile.json")
+
+beforeEach(() => {
+  req = httpMocks.createRequest()
+  res = httpMocks.createResponse()
+  next = null
+  res.locals.user = locals
+  req.file.location = photoFile
+})
+
+User.findOne = jest.fn()
+User.create = jest.fn()
+User.updateOne = jest.fn()
+Family.findOne = jest.fn()
+FamilyMember.find = jest.fn()
+FamilyMember.updateMany = jest.fn()
+Event.updateMany = jest.fn()
+MissionMember.updateMany = jest.fn()
+VoiceFile.updateMany = jest.fn()
+
+describe("유저 조회", async () => {
+  test("유저 정보 조회", () => {
+    const { user } = locals
+    user.password = "";
+
+    const familyChk = await FamilyMember.find.mockResolvedValue(isMember)
+      let familyList = []
+      if (familyChk.length) {
+        for (let family of familyChk) {
+          const chechkedFamily = await Family.findOne.mockResolvedValue(isFamily)
+          familyList.push(chechkedFamily)
+        }
+      }
+
+    await userController.getUser(req, res, next)
+    expect(res._getJSONData()).toStrictEqual({
+        user,
+        familyList,
+      })
+  })
+
+  test("유저 프로필 조회", async () => {
+    const { email } = locals
+    const userInfo = await User.findOne(isUser)
+    const nickname = userInfo.nickname;
+    const profileImg = userInfo.profileImg;
+    const todayMood = userInfo.todayMood;
+
+    await userController.getProfile(req, res, next)
+    expect(res._getJSONData()).toStrictEqual({
+      nickname,
+      profileImg,
+      todayMood,
+      })
+  })
+})
+
+//photoFile 파일의 들어오는 형태 알아보기.
+describe("유저 데이터 편집", async() => {
+
+    test("유저 프로필 수정", async() => {
+        const { userId } = res.locals.userId
+        
+    await userController.editProfile(req, res, next)
+    expect(res._getJSONData()).toStrictEqual({
+      nickname,
+      profileImg,
+      todayMood,
+      })
+    })
+})
